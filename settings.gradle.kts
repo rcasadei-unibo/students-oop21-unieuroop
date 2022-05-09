0be21115bc70c260ac6a8cbe071997ba3da4ec92
Roberto Casadei <robyonrails@gmail.com>
2022-05-09 12:50:12 +0200

    oop machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,26 @@
  * in the user manual at https://docs.gradle.org/7.4/userguide/multi_project_builds.html
  */
 
+ import org.danilopianini.VersionAliases.justAdditionalAliases
+
+ plugins {
+     id("de.fayard.refreshVersions") version "0.40.1"
+ }
+
+ refreshVersions {
+     extraArtifactVersionKeyRules = justAdditionalAliases
+ }
+
+ buildscript {
+     repositories {
+         gradlePluginPortal()
+         mavenCentral()
+     }
+     dependencies {
+         classpath("org.danilopianini:refreshversions-aliases:+")
+     }
+ }
+
+
 rootProject.name = "unieuroop"
 include("app")
